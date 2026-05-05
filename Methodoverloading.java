b2b6ecbef22221b181689c436622f5ed9bab1586
VenkateshGopagani <venkateshg.charani@gmail.com>
2026-05-05 15:28:37 +0530

    Add files via upload

---
+++ Methodoverloading.java
@@ -0,0 +1,23 @@
+package javaprogram;
+  class Adder
+  {
+ static int add(int a,int b)
+  {
+	   return a+b;
+  }
+   static int add(int a,int b,int c)
+  {
+	   return a+b+c;
+  }
+  }
+
+public class Methodoverloading {
+
+	public static void main(String[] args) {
+		System.out.println(Adder.add(11,11));
+		System.out.println(Adder.add(11,11,11));
+		
+
+	}
+
+}
